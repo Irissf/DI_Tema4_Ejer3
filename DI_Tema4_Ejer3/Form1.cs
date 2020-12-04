e02d30671dacebdf108b508e16a3b0919385506f
Irissf <iris.seijo@gmail.com>
2020-12-04 19:28:51 +0100

    Agregar archivos de proyecto.

---
+++ DI_Tema4_Ejer3/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DI_Tema4_Ejer3
+{
+    public partial class Form1 : Form
+    {
+        private string path;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Visor de imagenes";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+
+                ofd.InitialDirectory = "c:\\";
+                ofd.Filter = "Todos los archivos(*.*)|*.*|Imagen png (*.png)|*.png| Imagen jpg(*jpg)|*.jpg";
+                ofd.FilterIndex = 1;
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    path = ofd.FileName; //saca el path entero
+                    if (Path.GetExtension(ofd.FileName) == ".png" || Path.GetExtension(ofd.FileName) == ".jpg")
+                    {
+                        Form2 f2 = new Form2();
+                        f2.Text = Path.GetFileNameWithoutExtension(ofd.FileName);
+                        f2.pictureBox1.Image = new Bitmap(path);
+                        f2.Size = new Size(f2.pictureBox1.Image.Width, f2.pictureBox1.Image.Height);
+                        if (checkBox1.Checked)
+                        {
+                            f2.ShowDialog();
+                        }
+                        else
+                        {
+                            f2.Show();
+                        }
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("No es una imagen");
+                    }
+
+                }
+            }
+        }
+
+        private void salir(object sender, FormClosingEventArgs e)
+        {
+
+            if (MessageBox.Show("¿Seguro que deseas salir de la aplicación?", "Mi Aplicación",
+             MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+
+        }
+    }
+}
---
+++ DI_Tema4_Ejer3/Form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DI_Tema4_Ejer3
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            //if (this.pictureBox1.Width > this.Width || this.pictureBox1.Height > this.Height ||
+            //   this.pictureBox1.Width < this.Width || this.pictureBox1.Height < this.Height)
+            //{
+            //    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            //}
+        }
+    }
+}
